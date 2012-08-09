c691755b3a158a63bc7a99afb1fc032bb372d406
Navi Arimeka <wrk.navi@gmail.com>
2012-08-09 11:24:41 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NicoChart::Application.config.secret_token = '03c887f64c51d8f9dc353b0392e823f36637928ecc4912cf9dd5809bbc3e255444d3d111c1a482a8765af791d07de27a722bf405f4cf92d3f0f734b7260b1b30'
