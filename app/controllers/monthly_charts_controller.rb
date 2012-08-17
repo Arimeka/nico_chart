d88502a941e23960657e8541e03eee071ba0efe7
Navi Arimeka <wrk.navi@gmail.com>
2012-08-17 16:49:26 +0400

    Delet controllers cache

--- app/controllers/monthly_charts_controller.rb
+++ app/controllers/monthly_charts_controller.rb
@@ -1,17 +1,17 @@
 class MonthlyChartsController < ApplicationController
   def favorites
-  	@chart = MonthlyChart.all_caching("fav DESC", 100)
+  	@chart = MonthlyChart.all(order: "fav DESC", limit: 100)
   end
 
   def views
-  	@chart = MonthlyChart.all_caching("view DESC", 100)
+  	@chart = MonthlyChart.all(order: "view DESC", limit: 100)
   end
 
   def comments
-  	@chart = MonthlyChart.all_caching("comment DESC", 100)
+  	@chart = MonthlyChart.all(order: "comment DESC", limit: 100)
   end
 
   def mylist
-  	@chart = MonthlyChart.all_caching("mylist DESC", 100)
+  	@chart = MonthlyChart.all(order: "mylist DESC", limit: 100)
   end
 end
